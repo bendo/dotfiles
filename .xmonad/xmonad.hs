7a5df7032a7da73764bfdbc5f74482cbc6b34235
bendo <bendo.dev@skiff.com>
2017-10-03 18:49:54 +0200

    Update xmonad setup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,7 +6,9 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.Fullscreen
+import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Spiral
 import XMonad.Layout.Tabbed
 import XMonad.Util.Run(spawnPipe)
@@ -15,110 +17,116 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
 main = do
-	xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
-	xmonad $ defaults
-		{ logHook = dynamicLogWithPP $ xmobarPP
-			{ ppOutput = hPutStrLn xmproc
-			, ppTitle = xmobarColor colorBlue "" . shorten 100
-			, ppCurrent = xmobarColor colorBlue ""
-			, ppSep = " "
-			}
-		, manageHook = manageDocks <+> myManageHook
-		, startupHook = setWMName "LG3D"
-		}
+    din <- spawnPipe "xmobar"
+    xmonad $ docks $ defaults din
 
-defaults = defaultConfig
-	{ terminal 		= "urxvtc"
-	, focusFollowsMouse 	= True
-	, borderWidth 		= 1
-	, modMask 		= mod4Mask
-	, workspaces 		= myWorkspaces
-	, normalBorderColor 	= colorGray
-	, focusedBorderColor 	= colorGreen
-	, keys 			= myKeys
-	, mouseBindings 	= myMouseBindings
-	, layoutHook 		= smartBorders $ myLayout
-	, manageHook 		= myManageHook
-	, startupHook 		= return ()
-	}
+myLogHook h = dynamicLogWithPP xmobarPP
+    { ppHidden = xmobarColor "white" ""
+    , ppOutput = hPutStrLn h
+    , ppTitle = xmobarColor "blue" ""
+    }
 
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+myStartupHook :: X ()
+myStartupHook = do
+    spawn "xmobar ~/.xmobarrc"
 
-myManageHook = composeAll									-- xprop | grep WM_CLASS
-	[ className	=? "Google-chrome"	--> doShift "2"
-	, className 	=? "Java Console"	--> doShift "8"
-	, resource 	=? "desktop_window"	--> doIgnore
-	, className	=? "Google-chrome"	--> doFloat
-	, className 	=? "Java Console"	--> doFloat
-	, className 	=? "Galculator"		--> doFloat
-	, className 	=? "Steam"		--> doFloat
-	, className 	=? "Gimp"		--> doFloat
-	, resource 	=? "gpicview"		--> doFloat
-	, isFullscreen	--> (doF W.focusDown <+> doFullFloat)]
+defaults din = def
+    { terminal           = "urxvtc"
+    , focusFollowsMouse  = False
+    , borderWidth        = 1
+    , normalBorderColor  = colorGray
+    , focusedBorderColor = colorGreen
+    , modMask            = mod4Mask
+    , logHook            = myLogHook din
+    , workspaces         = myWorkspaces
+    , keys               = myKeys
+    , mouseBindings      = myMouseBindings
+    , layoutHook         = myLayouts
+    , manageHook         = myManageHook
+    , startupHook        = myStartupHook
+    }
 
-myLayout = avoidStruts (
-	Full |||
---	tabbed shrinkText tabConfig |||
-	Tall 1 (3/100) (1/2) |||
-	Mirror (Tall 1 (3/100) (1/2))) |||
---	spiral (6/7)) |||
-	noBorders (fullscreenFull Full)
+myWorkspaces = ["1", "2", "code", "web"] ++ map show [5..9]
 
-colorBlack	= "#020202"
-colorGray	= "#7c7c7c"
-colorGreen	= "#5bce2d"
-colorWhite	= "#eeeeee"
-colorBlue	= "#7bb8e2"
+myManageHook = composeAll
+    [ className =? "Google-chrome"     --> doShift "2"
+    , className =? "Java Console"      --> doShift "8"
+    , resource  =? "desktop_window"    --> doIgnore
+    , className =? "Google-chrome"     --> doFloat
+    , className =? "Java Console"      --> doFloat
+    , className =? "Galculator"        --> doFloat
+    , className =? "Steam"             --> doFloat
+    , className =? "Gimp"              --> doFloat
+    , resource  =? "gpicview"          --> doFloat
+    , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
 
-tabConfig = defaultTheme 
-	{ activeBorderColor	= colorGray
-	, activeTextColor	= colorBlue
-	, activeColor		= colorBlack
-	, inactiveBorderColor	= colorGray
-	, inactiveTextColor	= colorWhite
-	, inactiveColor		= colorBlack
-	}
+myLayouts = onWorkspace "web" webLayout $ standardLayout
+    where
+        standardLayout = avoidStruts ( Mirror tall ||| tall ||| Grid ||| Full )
+            where
+                tall = Tall nmaster delta ratio
+                nmaster = 1
+                ratio = 1/2
+                delta = 2/100
+        webLayout = avoidStruts $ Mirror tall --layout for browser and terminal window
+            where
+                tall = Tall nmaster delta ratio --define tall layout sizes
+                nmaster = 1 --number of windows in master pane1
+                ratio = 3/4 --ratio of master pane size
+                delta = 2/100
+
+colorBlack = "#020202"
+colorGray  = "#7c7c7c"
+colorGreen = "#5bce2d"
+colorWhite = "#eeeeee"
+colorBlue  = "#7bb8e2"
+
+tabConfig = def
+    { activeBorderColor   = colorGray
+    , activeTextColor     = colorBlue
+    , activeColor         = colorBlack
+    , inactiveBorderColor = colorGray
+    , inactiveTextColor   = colorWhite
+    , inactiveColor       = colorBlack
+    }
 
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-	[ ((modMask, xK_e), spawn $ XMonad.terminal conf)					-- Terminal
-	, ((modMask, xK_i), spawn "idea")							-- IntelliJ IDEA
-	, ((modMask, xK_g), spawn "google-chrome")						-- Chrome
-	, ((modMask, xK_f), spawn "firefox")							-- Firefox
-	, ((modMask, xK_x), spawn "xkill")							-- X kill
-	, ((modMask .|. shiftMask, xK_l), spawn "gnome-screensaver-command --lock")		-- Lock with gnome
-	, ((modMask, xK_d), spawn "dmenu_run -nb '#000000' -nf '#aaaaaa' -sb green -sf black -fn 'terminus-8' -b")
-	, ((modMask, xK_q), kill)								-- Close focused window.
-	, ((modMask, xK_space), sendMessage NextLayout)						-- Cycle through the available layout algorithms.
-	, ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)		-- Reset the layouts on the current workspace to default.
-	, ((modMask, xK_n), refresh)								-- Resize viewed windows to the correct size.
-	, ((modMask, xK_Tab), windows W.focusDown)						-- Move focus to the next window.
-	, ((modMask, xK_j), windows W.focusDown)						-- Move focus to the next window.
-	, ((modMask, xK_k), windows W.focusUp)							-- Move focus to the previous window.
-	, ((modMask, xK_m), windows W.focusMaster)						-- Move focus to the master window.
-	, ((modMask, xK_Return), windows W.swapMaster)						-- Swap the focused window and the master window.
-	, ((modMask .|. shiftMask, xK_j), windows W.swapDown)					-- Swap the focused window with the next window.
-	, ((modMask .|. shiftMask, xK_k), windows W.swapUp)					-- Swap the focused window with the previous window.
-	, ((modMask, xK_h), sendMessage Shrink)							-- Shrink the master area.
-	, ((modMask, xK_l), sendMessage Expand)							-- Expand the master area.
-	, ((modMask, xK_t), withFocused $ windows . W.sink)					-- Push window back into tiling.
-	, ((modMask, xK_comma), sendMessage (IncMasterN 1))					-- Increment the number of windows in the master area.
-	, ((modMask, xK_period), sendMessage (IncMasterN (-1)))					-- Decrement the number of windows in the master area.
-	, ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))				-- Quit xmonad.
-	, ((modMask, xK_r), restart "xmonad" True)						-- Restart xmonad.
-	, ((0, xK_Print), spawn "/usr/bin/scrot '%Y-%m-%d_$wx$h.png'") 
-	] ++
-	[ ((m .|. modMask, k), windows $ f i)
-	    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-	    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-	] ++
-	[ ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-	    | (key, sc) <- zip [xK_u, xK_o, xK_p] [0..]
-	    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
-	]
- 
+    [ ((modMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modMask, xK_x), spawn "xkill")
+    , ((modMask .|. shiftMask, xK_l), spawn "sh ~/.xmonad/lock.sh")
+    , ((modMask, xK_d), spawn "dmenu_run -fn '-9'")
+    , ((modMask, xK_q), kill)
+    , ((modMask, xK_space), sendMessage NextLayout)
+    , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+    , ((modMask, xK_n), refresh)
+    , ((modMask, xK_Tab), windows W.focusDown)
+    , ((modMask, xK_j), windows W.focusDown)
+    , ((modMask, xK_k), windows W.focusUp)
+    , ((modMask, xK_m), windows W.focusMaster)
+    --, ((modMask, xK_Return), windows W.swapMaster)
+    , ((modMask .|. shiftMask, xK_j), windows W.swapDown)
+    , ((modMask .|. shiftMask, xK_k), windows W.swapUp)
+    , ((modMask, xK_h), sendMessage Shrink)
+    , ((modMask, xK_l), sendMessage Expand)
+    , ((modMask, xK_t), withFocused $ windows . W.sink)
+    , ((modMask, xK_comma), sendMessage (IncMasterN 1))
+    , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+    , ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+    , ((modMask, xK_r), restart "xmonad" True)
+    , ((0, xK_Print), spawn "scrot '%Y-%m-%d-%T_$wx$h.png' -e 'mv $f ~'")
+    ] ++
+    [ ((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ] ++
+    [ ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_u, xK_o, xK_p] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
-	[ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))				-- mod-button1, Set the window to floating mode and move by dragging
-	, ((modMask, button2), (\w -> focus w >> windows W.swapMaster))				-- mod-button2, Raise the window to the top of the stack
-	, ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))				-- mod-button3, Set the window to floating mode and resize by dragging
-	]											-- bind events to the mouse scroll wheel (button4 and button5)
- 
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    ]
+
