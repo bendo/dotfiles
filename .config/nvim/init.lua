e41aafa4a9b9cd90728697dadeb7b108b3098af6
bendo <bendo.dev@skiff.com>
2023-07-08 14:28:55 +0200

    Add haskell grammar for treesiter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,7 @@
 -- bendo
 
+-- use :Mason to add LSP
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -258,7 +260,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'html', 'java', 'javascript', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'lua', 'python', 'rust', 'haskell', 'html', 'java', 'javascript', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
