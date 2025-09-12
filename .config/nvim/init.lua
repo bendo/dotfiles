b328b7d7a345ee25df7b4b2900de9a3343c0b667
bedno <bendo.dev@skiff.com>
2025-09-12 16:08:49 +0200

    Fix nvim error

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -494,16 +494,6 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
-
 -- nvim-cmp setup
 local luasnip = require 'luasnip'
 local cmp = require 'cmp'
