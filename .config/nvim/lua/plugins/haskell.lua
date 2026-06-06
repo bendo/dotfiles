6f07cf5864569c1676a1c06b21d5cffa056985e5
bedno <bendo.dev@tuta.io>
2026-06-06 14:37:31 +0200

    Fix nvim config

--- .config/nvim/lua/plugins/haskell.lua
+++ .config/nvim/lua/plugins/haskell.lua
@@ -1,3 +1,15 @@
 return {
   'haskell/stylish-haskell',
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      vim.lsp.config('hls', {
+        cmd = {
+          '/home/bendo/.ghcup/hls/2.13.0.0/bin/haskell-language-server-9.12.2',
+          '--lsp',
+        },
+      })
+      vim.lsp.enable('hls')
+    end,
+  },
 }
