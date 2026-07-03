9ea5182dcb011bca46c6a73064ae1866083e8313
bedno <bendo.dev@tuta.io>
2026-07-03 18:14:12 +0200

    Fix lsp keymaps

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,25 +1,35 @@
 local keymap = vim.keymap
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
-    keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition'})
-    keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Go to implementation'})
-    keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    keymap.set('n', '<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-    keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, { desc = 'Format file'})
-    keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    local function map(mode, lhs, rhs, desc)
+      keymap.set(mode, lhs, rhs, {
+        buffer = ev.buf,
+        desc = desc,
+      })
+    end
+
+    map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
+    map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+    map('n', 'K', vim.lsp.buf.hover, 'Hover documentation')
+    map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
+    map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, 'Add workspace folder')
+    map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, 'Remove workspace folder')
+    map('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, 'List workspace folders')
+    map('n', '<space>D', vim.lsp.buf.type_definition, 'Go to type definition')
+    map('n', '<space>rn', vim.lsp.buf.rename, 'Rename symbol')
+    map('n', 'gr', vim.lsp.buf.references, 'Find references')
+    map('n', '<space>f', function()
+      vim.lsp.buf.format({ async = true })
+    end, 'Format file')
+    map({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, 'Code action')
   end
 })
